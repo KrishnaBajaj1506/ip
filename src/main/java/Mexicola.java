6199efd08cb211381e2efab20c1815cf2caec65d
Krishna <krishnabajaj1506@gmail.com>
2026-01-27 18:05:20 +0800

    Implement Level 3: Mark as Done

--- src/main/java/Mexicola.java
+++ src/main/java/Mexicola.java
@@ -5,7 +5,7 @@ public class Mexicola {
         String line = "____________________________________________________________";
 
         // max 100 task
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100]; // Array of Task objects now
         int taskCount = 0; // Keeps track of how many tasks are actually in the array
 
         System.out.println(line);
@@ -25,15 +25,27 @@ public class Mexicola {
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
                 System.out.println(line);
+                System.out.println(line + "\n Here are the tasks in your list:");
                 // Only loop up to the current number of tasks added
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i].toString());
                 }
                 System.out.println(line);
-            } else {
+            }
+            else if (userInput.startsWith("mark ")) {
+                // Extracts the number and converts to 0-based index
+                int idx = Integer.parseInt(userInput.substring(5)) - 1;
+                tasks[idx].markAsDone();
+                System.out.println(line + "\n Nice! I've marked this task as done:\n   " + tasks[idx] + "\n" + line);
+            } else if (userInput.startsWith("unmark ")) {
+                int idx = Integer.parseInt(userInput.substring(7)) - 1;
+                tasks[idx].unmark();
+                System.out.println(line + "\n OK, I've marked this task as not done yet:\n   " + tasks[idx] + "\n" + line);
+            }
+            else {
                 // Check if there is still space in the array
                 if (taskCount < 100) {
-                    tasks[taskCount] = userInput;
+                    tasks[taskCount] = new Task(userInput);
                     taskCount++;
                     System.out.println(line);
                     System.out.println(" added: " + userInput);
