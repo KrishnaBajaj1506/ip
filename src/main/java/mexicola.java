0a3f01b66d8117014e6825d0dc93c2ea2b0b6fb3
Krishna <krishnabajaj1506@gmail.com>
2026-01-27 17:15:38 +0800

    Implement Level 1: Echo functionality

--- src/main/java/mexicola.java
+++ src/main/java/mexicola.java
@@ -1,7 +1,27 @@
+import java.util.*;
 public class mexicola {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
         System.out.println("Hello! I'm Mexicola");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+
+            // Check for exit condition
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            // Echo the command
+            System.out.println(line);
+            System.out.println(" " + userInput);
+            System.out.println(line);
+        }
     }
 }
